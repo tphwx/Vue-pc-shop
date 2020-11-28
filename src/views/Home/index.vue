50e09d20bfd39bf0b948a4eb182e6dd4f8e43531
tphwx <825462650@qq.com>
2020-11-28 14:45:47 +0800

    faet:完成home静态组件

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,14 +1,45 @@
 <template>
   <div>
-    <div>Home...</div>
+    <TypeNav />
+    <!--列表-->
+    <ListContainer />
+    <!--今日推荐-->
+    <TodayRecommend />
+    <!-- 商品排行 -->
+    <Rank />
+    <!-- 猜你喜欢 -->
+    <Like />
+    <!--楼层-->
+    <Floor />
+    <!--楼层-->
+    <Floor />
+    <!--商标-->
+    <Brand />
   </div>
 </template>
 
 <script>
-export default {
-  name: "Home",
-};
+  import Brand from './Brand/Brand'
+  import Floor from './Floor/Floor'
+  import Like from './Like/Like'
+  import ListContainer from './ListContainer/ListContainer'
+  import Rank from './Rank/Rank'
+  import TodayRecommend from './TodayRecommend/TodayRecommend'
+  import TypeNav from './TypeNav/index'
+  export default {
+    name: 'Home',
+    components: {
+      Brand,
+      Floor,
+      Like,
+      ListContainer,
+      Rank,
+      TodayRecommend,
+      TypeNav
+    }
+  }
 </script>
 
 <style lang="less" scoped>
+
 </style>
